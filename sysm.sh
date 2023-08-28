e9a45ea82cfbc9b515a335d90ba157a55dd1fcda
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-28 22:53:23 +0000

    Update sysm.sh

--- sysm.sh
+++ sysm.sh
@@ -4,8 +4,23 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Display a welcome message
-echo "System Maintenance Script"
+# Function to display a colored message
+print_colored_message() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+# Function to show the welcome menu
+show_welcome_menu() {
+    clear
+    echo "===== System Maintenance Script ====="
+    echo "1. Update package information"
+    echo "2. Upgrade installed packages"
+    echo "3. Remove unnecessary packages"
+    echo "4. Clean up temporary files"
+    echo "5. Check disk space usage"
+    echo "0. Exit"
+    echo "====================================="
+}
 
 # Function to ask for user confirmation
 confirm_action() {
@@ -17,36 +32,50 @@ confirm_action() {
     esac
 }
 
-# Function to display a colored message
-print_colored_message() {
-    echo -e "${GREEN}$1${NC}"
-}
+# Main menu loop
+while true; do
+    show_welcome_menu
+    read -p "Enter your choice (0-5): " user_choice
 
-# Ask for confirmation before each action
+    case "$user_choice" in
+        1)
+            if confirm_action "updating package information"; then
+                sudo apt update
+                print_colored_message "Package information updated."
+            fi
+            ;;
+        2)
+            if confirm_action "upgrading installed packages"; then
+                sudo apt upgrade -y
+                print_colored_message "Installed packages upgraded."
+            fi
+            ;;
+        3)
+            if confirm_action "removing unnecessary packages"; then
+                sudo apt autoremove -y
+                print_colored_message "Unnecessary packages removed."
+            fi
+            ;;
+        4)
+            if confirm_action "cleaning up temporary files"; then
+                sudo rm -rf /tmp/*
+                print_colored_message "Temporary files cleaned up."
+            fi
+            ;;
+        5)
+            if confirm_action "checking disk space usage"; then
+                df -h
+                print_colored_message "Disk space usage checked."
+            fi
+            ;;
+        0)
+            echo "Exiting. Have a great day!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please enter a valid option (0-5)."
+            ;;
+    esac
 
-if confirm_action "updating package information"; then
-    sudo apt update
-    print_colored_message "Package information updated."
-fi
-
-if confirm_action "upgrading installed packages"; then
-    sudo apt upgrade -y
-    print_colored_message "Installed packages upgraded."
-fi
-
-if confirm_action "removing unnecessary packages"; then
-    sudo apt autoremove -y
-    print_colored_message "Unnecessary packages removed."
-fi
-
-if confirm_action "cleaning up temporary files"; then
-    sudo rm -rf /tmp/*
-    print_colored_message "Temporary files cleaned up."
-fi
-
-if confirm_action "checking disk space usage"; then
-    df -h
-    print_colored_message "Disk space usage checked."
-fi
-
-print_colored_message "System maintenance complete."
+    read -p "Press Enter to continue..."
+done
